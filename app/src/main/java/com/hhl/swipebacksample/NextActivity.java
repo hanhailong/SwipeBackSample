e6667632c91447f2567289f68441b61263caf9f4
HailongHan <hailonghan@HailongHantekiMacBook-Pro.local>
2015-06-09 22:07:53 +0800

    Initial commit

---
+++ app/src/main/java/com/hhl/swipebacksample/NextActivity.java
@@ -0,0 +1,58 @@
+package com.hhl.swipebacksample;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import java.util.Random;
+
+public class NextActivity extends BaseSwipeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+
+        RelativeLayout containerRl = (RelativeLayout) findViewById(R.id.container);
+
+        //随机色
+
+        Random random = new Random();
+        int red = random.nextInt(255);
+        int green = random.nextInt(255);
+        int blue = random.nextInt(255);
+
+        containerRl.setBackgroundColor(Color.argb(255,red,green,blue));
+
+    }
+
+    public void nextPage(View v) {
+        startActivity(new Intent(this, NextActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_next, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
